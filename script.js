66ae040bf5d64811c634e88bb949c5198ea9370f
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2026-05-30 15:43:33 -0500

    refactor: enhance createBoard function with square size calculations

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const board = document.querySelector('.board');
 
+
+
 function createBoard(size) {
+    const boardSize = 400; // Size of the board in pixels
+    const squareSize = boardSize / size; // Size of each square
+  
+
     board.innerHTML = '';
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'black';
+            
         });
         board.appendChild(square);
     }
 }
 
-createBoard(16);   
+createBoard(16  );   
